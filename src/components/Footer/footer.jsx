cc0419ad92ad63bb570dd029d649245845156596
Iva Havranová <havranova.i@seznam.cz>
2023-06-03 18:32:34 +0200

    Footer created

--- src/components/Footer/footer.jsx
+++ src/components/Footer/footer.jsx
@@ -4,7 +4,11 @@ import './footer.css';
 export const Footer = () => {
   return (
     <footer>
-      <p>Ahoj</p>
+      <p className="footer--text">
+        Projekt Hike it and like it vytvořen v rámci Digitální akademie - web
+        2023, Czechitas
+      </p>
+      <p className="footer--text">Michaela trčková a Iva Havranová</p>
     </footer>
   );
 };
