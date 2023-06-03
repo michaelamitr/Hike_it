525907a3d3ed87df27bea058bea5c0b6c23e6fec
Iva Havranová <havranova.i@seznam.cz>
2023-06-03 22:43:59 +0200

    MainPage done

--- src/components/Footer/footer.jsx
+++ src/components/Footer/footer.jsx
@@ -5,8 +5,7 @@ export const Footer = () => {
   return (
     <footer>
       <p className="footer--text">
-        Projekt Hike it and like it vytvořen v rámci Digitální akademie - web
-        2023, Czechitas
+        Projekt Hike it and like it - Digitální akademie - web 2023, Czechitas
       </p>
       <p className="footer--text">Michaela trčková a Iva Havranová</p>
     </footer>
--- src/pages/MainPage/mainpage.jsx
+++ src/pages/MainPage/mainpage.jsx
@@ -10,42 +10,21 @@ export const MainPage = () => {
           <span className="likeit">...and like it</span>
         </div>
       </div>
-      <section className="main__text">
-        <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorem
-          accusamus minima laborum, adipisci nisi iure consequuntur totam illo
-          suscipit reiciendis quo ducimus quae ratione placeat unde delectus
-          atque quibusdam. Exercitationem? Ducimus provident odio numquam culpa
-          aperiam assumenda modi nihil, libero, esse ea, pariatur veniam
-          corporis repellendus quos. Laudantium impedit, rerum reprehenderit
-          eius tenetur debitis, iure vel necessitatibus assumenda ea doloribus.
-          Consectetur similique totam vel voluptate a? Consequuntur esse
-          architecto quos labore tenetur eligendi! Minus, nam vel odit fugit
-          doloremque velit aliquam sit officia impedit, ea magni. Corrupti quod
-          ipsa sapiente. Odit odio tenetur impedit dolorum quas nulla laboriosam
-          iste aliquam mollitia rem, maxime aperiam veniam totam perferendis
-          repellendus. Culpa, dolore? In, voluptas odit. Hic, debitis pariatur
-          ab obcaecati libero in. Natus voluptates tempore, incidunt repellendus
-          aperiam dolorem, quia corrupti magnam tenetur quod architecto maiores
-          nihil suscipit esse voluptatum distinctio molestiae molestias eos.
-          Sequi, omnis maxime repudiandae ipsa ea possimus laboriosam? Vitae
-          esse aut maiores fugit sequi non deserunt repellat quos voluptatum
-          iusto. Maiores, nam beatae quas minima quam itaque sequi nisi
-          doloremque rem deleniti. Incidunt eum inventore placeat totam aut.
-          Tempore vel, modi, vero ad qui libero laboriosam placeat voluptatem
-          possimus nemo sit aliquid hic, voluptate expedita debitis? Qui impedit
-          aut, porro tempore dignissimos distinctio asperiores nam a dolorem
-          earum! Corrupti necessitatibus qui ipsam deleniti voluptas id
-          voluptatum earum in accusamus officiis provident aspernatur, rem
-          cupiditate quasi officia praesentium velit iure illo libero. Alias,
-          voluptatum obcaecati sed eius nostrum animi? Provident exercitationem
-          beatae assumenda, quidem, excepturi illum atque, officia libero dolore
-          impedit id voluptate deserunt corrupti quaerat. Laudantium neque
-          architecto, expedita cumque quidem, nihil natus in, nisi eius incidunt
-          eum. Velit rerum asperiores, quia cupiditate quidem voluptatum
-          aspernatur vero excepturi eum. Consequuntur omnis cupiditate commodi
-          incidunt, quibusdam rerum odit perferendis quis exercitationem amet
-          itaque non veniam. Dicta suscipit nostrum ab.
+      <section className="main__section">
+        <p className="main__text">
+          Vítejte na naší webové stránce věnované dlouhým vzdálenostním trasám.
+          Pokud máte rádi pěší turistiku a cestování, určitě vás budou zajímat
+          informace o nejkrásnějších a nejzajímavějších dálkových trasách, které
+          na světě existují. Tyto trasy nabízejí nejen možnost poznat krásy
+          přírody, ale také vás provedou historickými a kulturními památkami. Na
+          naší stránce se zaměřujeme na představení dálkových tras, které nás
+          zaujaly, nebo které jsme sami absolvovali. Naším cílem je poskytnout
+          vám inspiraci pro vaše pěší výlety a pomoci vám naplánovat vaši
+          vlastní dálkovou túru. Na této stránce se dozvíte více o jednotlivých
+          trasách, včetně délky, obtížnosti, zajímavostí, které po cestě můžete
+          vidět, a dalších užitečných informacích pro vaši cestu. Přejeme vám
+          příjemné prohlížení našich stránek a mnoho nezapomenutelných zážitků
+          na vašich budoucích túrách!
         </p>
       </section>
     </section>
