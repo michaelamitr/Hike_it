67b4b86c3e2322fb43e7eee1e807082617c64bb9
Iva Havranová <havranova.i@seznam.cz>
2023-06-04 12:15:29 +0200

    All trails in progress

---
+++ src/pages/AllTrails/Fish/fish.jsx
@@ -0,0 +1,6 @@
+import React from 'react';
+import './fish.css';
+
+export const Fish = () => {
+  return <section id="fish__trail"></section>;
+};
--- src/pages/AllTrails/alltrails.jsx
+++ src/pages/AllTrails/alltrails.jsx
@@ -4,7 +4,14 @@ import './alltrails.css';
 export const AllTrails = () => {
   return (
     <section id="all-trails">
-      <div>All Trails Page</div>
+      <div className="fish">
+        <h1 className="fish__title">Rybářská stezka</h1>
+      </div>
+      <div className="fish__map">tady bude mapa</div>
+      <div className="gr">
+        <h1 className="gr__title">GR221</h1>
+      </div>
+      <div className="gr__map">tady bude mapa</div>
     </section>
   );
 };
