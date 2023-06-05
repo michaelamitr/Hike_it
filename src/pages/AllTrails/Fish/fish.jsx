d323a0c2f4879adaa5ec4be08b5d2cdec49bfbc8
Iva Havranová <havranova.i@seznam.cz>
2023-06-05 21:03:37 +0200

    navigation for mobile in trail component in progress

--- src/pages/AllTrails/Fish/fish.jsx
+++ src/pages/AllTrails/Fish/fish.jsx
@@ -2,5 +2,21 @@ import React from 'react';
 import './fish.css';
 
 export const Fish = () => {
-  return <section id="fish__trail">Fish zde</section>;
+  return (
+    <section id="fish__trail">
+      <div className="fish__page">
+        <div className="fish__banner">
+          <div className="title">
+            <h1 className="title__heading">Rybářská stezka</h1>
+          </div>
+        </div>
+        <nav className="navigation">
+          <a href="#">Obecné info</a>
+          <a href="#">Etapy</a>
+          <a href="#">Co s sebou</a>
+          <a href="#">Galerie</a>
+        </nav>
+      </div>
+    </section>
+  );
 };
