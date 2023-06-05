c8ac0eadffb5a98d3de62fbd6e82dbe6a21fc978
Iva Havranová <havranova.i@seznam.cz>
2023-06-05 21:47:12 +0200

    classname change

--- src/pages/AllTrails/alltrails.jsx
+++ src/pages/AllTrails/alltrails.jsx
@@ -5,18 +5,18 @@ import { NavLink } from 'react-router-dom';
 export const AllTrails = () => {
   return (
     <section id="all-trails">
-      <div className="fish">
+      <div className="trail">
         <NavLink to="/fishermans-trail">
-          <h1 className="fish__title">Rybářská stezka</h1>
+          <h1 className="trail__title">Rybářská stezka</h1>
         </NavLink>
       </div>
-      <div className="fish__map">tady bude mapa</div>
-      <div className="gr">
+      <div className="trail__map">tady bude mapa</div>
+      <div className="trail">
         <NavLink to="/gr221">
-          <h1 className="gr__title">GR221</h1>
+          <h1 className="trail__title">GR221</h1>
         </NavLink>
       </div>
-      <div className="gr__map">tady bude mapa</div>
+      <div className="trail__map">tady bude mapa</div>
     </section>
   );
 };
