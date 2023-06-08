eac719690f982310f6a882c77631859977e4a85e
Michaela Trčková <misa.trckova@volny.cz>
2023-06-08 16:04:11 +0200

    pictures added, banner pictures edit

--- src/pages/AllTrails/alltrails.jsx
+++ src/pages/AllTrails/alltrails.jsx
@@ -5,13 +5,13 @@ import { NavLink } from 'react-router-dom';
 export const AllTrails = () => {
   return (
     <section id="all-trails">
-      <div className="trail">
+      <div className="trail-fish">
         <NavLink to="/fishermans-trail">
           <h1 className="trail__title">Rybářská stezka</h1>
         </NavLink>
       </div>
       <div className="trail__map">tady bude mapa</div>
-      <div className="trail">
+      <div className="trail-gr221">
         <NavLink to="/gr221">
           <h1 className="trail__title">GR221</h1>
         </NavLink>
