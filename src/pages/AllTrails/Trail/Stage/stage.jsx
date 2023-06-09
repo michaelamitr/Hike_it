eeb0edfed0973b9a51a984ae98496bcb4b8305ee
Iva Havranová <havranova.i@seznam.cz>
2023-06-09 21:07:30 +0200

    Stage css

--- src/pages/AllTrails/Trail/Stage/stage.jsx
+++ src/pages/AllTrails/Trail/Stage/stage.jsx
@@ -2,5 +2,14 @@ import React from 'react';
 import './stage.css';
 
 export const Stage = ({ number, name, distance, duration, description }) => {
-  return <></>;
+  return (
+    <div className="container">
+      <h1>
+        {number}. etapa: {name}
+      </h1>
+      <p>Vzdálenost: {distance}</p>
+      <p>Trvání: {duration}</p>
+      <p>{description}</p>
+    </div>
+  );
 };
