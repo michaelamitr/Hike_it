b164943a93027eeda55c44fafc6c780cced89376
Iva Havranová <havranova.i@seznam.cz>
2023-06-09 21:22:18 +0200

    stage css in progress

--- src/pages/AllTrails/Trail/Stage/stage.jsx
+++ src/pages/AllTrails/Trail/Stage/stage.jsx
@@ -1,12 +1,16 @@
 import React from 'react';
 import './stage.css';
+import cross from './img/cross.png'; //nefunguje mi
 
 export const Stage = ({ number, name, distance, duration, description }) => {
   return (
-    <div className="container">
-      <h1>
-        {number}. etapa: {name}
-      </h1>
+    <div className="stage">
+      <div className="stage__flex">
+        <h1 className="stage__title">
+          {number}. etapa: {name}
+        </h1>
+        <div className="stage__cross">{cross}</div>
+      </div>
       <p>Vzdálenost: {distance}</p>
       <p>Trvání: {duration}</p>
       <p>{description}</p>
--- src/pages/AllTrails/Trail/trail.jsx
+++ src/pages/AllTrails/Trail/trail.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import './trail.css';
 import { Checkbox } from './Checkbox/checkbox';
+import { Stage } from './Stage/stage';
 
 export const Trail = () => {
   return (
