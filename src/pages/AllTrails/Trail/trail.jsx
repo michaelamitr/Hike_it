d15efbf56fa532bc8d12a40d7e659fdb2c5fe63a
Iva Havranová <havranova.i@seznam.cz>
2023-06-10 20:18:25 +0200

    trail desktop fixed

--- src/pages/AllTrails/Trail/trail.jsx
+++ src/pages/AllTrails/Trail/trail.jsx
@@ -23,15 +23,25 @@ export const Trail = () => {
         </div>
       </div>
       <div className="trail__page">
-        <nav className="navigation">
-          <a href="#general">Obecné info</a>
-          <a href="#stages">Etapy</a>
-          <a href="#packing">Co s sebou</a>
-          <a href="#gallery">Galerie</a>
-        </nav>
+        <div className="trail__flex">
+          <nav className="navigation">
+            <a href="#general">Obecné info</a>
+            <a href="#stages">Etapy</a>
+            <a href="#packing">Co s sebou</a>
+            <a href="#gallery">Galerie</a>
+          </nav>
+          <div className="general__info general__info--desktop">
+            <p>Lokalita: {trailData.general.location}</p>
+            <p>Délka: {trailData.general.length}</p>
+            <p>Počet etap: {trailData.general.stagesNumber}</p>
+            <p>Typ: {trailData.general.type}</p>
+            <p>Obtížnost: {trailData.general.difficulty}</p>
+            <p>Vhodné absolvovat: {trailData.general.monthsToGo}</p>
+          </div>
+        </div>
         <section className="general" id="general">
           <Heading title="Obecné" url={trailData.sectionImages.general} />
-          <div className="general__info">
+          <div className="general__info general__info--mobile">
             <p>Lokalita: {trailData.general.location}</p>
             <p>Délka: {trailData.general.length}</p>
             <p>Počet etap: {trailData.general.stagesNumber}</p>
