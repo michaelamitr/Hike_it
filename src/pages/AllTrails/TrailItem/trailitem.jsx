394674166bdb54185c35940e4b653077eb3b4fe9
Michaela Trčková <misa.trckova@volny.cz>
2023-06-10 18:18:03 +0200

    map center gr221 edit

--- src/pages/AllTrails/TrailItem/trailitem.jsx
+++ src/pages/AllTrails/TrailItem/trailitem.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Heading } from '../Heading/heading';
-import { TrailMap } from '../Map/trailmap';
+import { TrailMap } from '../TrailMap/trailmap';
 import { Link } from 'react-router-dom';
 import './trailitem.css';
 
