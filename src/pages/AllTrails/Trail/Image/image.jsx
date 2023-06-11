0aa5bbb9cd77c18c000bab614f3610a3a78d123a
Iva Havranová <havranova.i@seznam.cz>
2023-06-11 12:46:04 +0200

    Gallery carousell in progress

--- src/pages/AllTrails/Trail/Image/image.jsx
+++ src/pages/AllTrails/Trail/Image/image.jsx
@@ -1,8 +1,12 @@
 import React from 'react';
 import './image.css';
 import { useRef } from 'react';
+import arrowLeft from './img/arrow-left.png';
+import arrowRight from './img/arrow-right.png';
+import { useState } from 'react';
 
 export const Image = ({ image }) => {
+  const [img, setImg] = useState(0);
   const dialogRef = useRef();
   const handleOpen = (e) => {
     dialogRef.current.showModal();
@@ -19,7 +23,16 @@ export const Image = ({ image }) => {
     <>
       <img onClick={handleOpen} className="gallery__img" src={image} alt="" />
       <dialog className="gallery__dialog" ref={dialogRef} onClick={handleClose}>
-        <img className="dialog__img" src={image} alt="" onClick={handleClose} />
+        <div className="dialog__content">
+          <img className="arrow arrow--left" src={arrowLeft} alt="" />
+          <img
+            className="dialog__img"
+            src={image}
+            alt=""
+            onClick={handleClose}
+          />
+          <img className="arrow arrow--right" src={arrowRight} alt="" />
+        </div>
       </dialog>
     </>
   );
