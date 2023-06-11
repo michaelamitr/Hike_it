e77c8958ddd7b23ff0814d7c69906d1f2094b122
Michaela Trčková <misa.trckova@volny.cz>
2023-06-11 18:50:18 +0200

    error page basic setup

--- src/components/ErrorPage/error.jsx
+++ src/components/ErrorPage/error.jsx
@@ -1,14 +1,20 @@
 import React from 'react';
 import './error.css';
+import { Header } from '../Header/header';
+import { Footer } from '../Footer/footer';
 
 export const ErrorPage = () => {
   return (
-    <section className="error">
-      <div className="error__page">
-        <h1>404: Tudy cesta nevede</h1>
-        <p>Vydali jste se na špatný trail.</p>
-        {/* <img src="" alt="" /> */}
-      </div>
-    </section>
+    <>
+      <Header />
+      <section className="error">
+        <div className="error__page">
+          <h1>404: Tudy cesta nevede</h1>
+          <p>Vydali jste se na špatný trail.</p>
+          {/* <img src="" alt="" /> */}
+        </div>
+      </section>
+      <Footer />
+    </>
   );
 };
