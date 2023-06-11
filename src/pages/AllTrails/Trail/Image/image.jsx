e0fc9b55a903c36e4a0277e8194ce8754406f800
Iva Havranová <havranova.i@seznam.cz>
2023-06-11 11:48:59 +0200

    gallery for mobile

--- src/pages/AllTrails/Trail/Image/image.jsx
+++ src/pages/AllTrails/Trail/Image/image.jsx
@@ -1,6 +1,26 @@
 import React from 'react';
 import './image.css';
+import { useRef } from 'react';
 
 export const Image = ({ image }) => {
-  return <img className="gallery__img" src={image} alt="" />;
+  const dialogRef = useRef();
+  const handleOpen = (e) => {
+    dialogRef.current.showModal();
+    e.preventDefault();
+  };
+
+  const handleClose = (e) => {
+    if (e.target === e.currentTarget) {
+      dialogRef.current.close();
+    }
+  };
+
+  return (
+    <>
+      <img onClick={handleOpen} className="gallery__img" src={image} alt="" />
+      <dialog className="gallery__dialog" ref={dialogRef} onClick={handleClose}>
+        <img className="dialog__img" src={image} alt="" onClick={handleClose} />
+      </dialog>
+    </>
+  );
 };
